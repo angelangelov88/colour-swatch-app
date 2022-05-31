dcae95c77cbac4afab9e4fee3e8a2ba116bcb3ff
Angel <82048582+angelangelov88@users.noreply.github.com>
2022-05-31 16:36:19 +0100

    Added a tooltip
    I added a tooltip with different options, such as:
     - theme - dark or light
     - selected - option to choose between hover or click
     - props for some basic text and a link
     - tooltipPosition - to position the elements based on the page. It can be left, right, top or bottom. The bottom option has 3 extra options
     - arrowPosition - position the arrow to the left, centre or right on the bottom
    Added conditionals to separate the different props and give the developer the option to choose their content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 // import { ColourThemeSwatch } from "./components/ColourThemeSwatch";
 import { IdentifierIcon } from "./components/IdentifierIcon";
+import { Tooltip } from "./components/Tooltip";
 
 function App() {
   return (
-    <div className="App bg-gray-30">
+    <div className="App m-32">
       {/* <ColourThemeSwatch color="primary" /> */}
-      <IdentifierIcon 
+      {/* <IdentifierIcon 
         color="purple"
-        size="md"
+        size="xs"
         type="initials"
         statusIndicator={true}
         status={true}
         initials="DM"
+      /> */}
+      <Tooltip
+        theme="dark"
+        selected="click"
+        infoHeader="Info Header"
+        infoText="Info Text"
+        link="https://test.com"
+        pointerPosition="bottom"
+        arrowPosition="centre"
       />
     </div>
   );
--- src/components/IdentifierIcon.js
+++ src/components/IdentifierIcon.js
@@ -15,8 +15,8 @@ const IdentifierIcon = (props) => {
   } = props
 
   const classes = classNames(
-    "font-ibm flex items-center justify-center rounded-full hover:cursor-pointer text-white uppercase relative",
-    size === "xs" && "w-8 h-8 text-xs", 
+    "flex items-center justify-center relative rounded-full font-ibm text-white uppercase",
+    size === "xs" && "w-8 h-8 text-xs",
     size === "sm" && "w-9 h-9 text-base",
     size === "md" && "w-10 h-10 text-lg",
     size === "lg" && "w-11 h-11 text-xl",
@@ -30,35 +30,23 @@ const IdentifierIcon = (props) => {
     color === "green" && "bg-green-60",
     color === "yellow" && "bg-yellow-60",
     color === "red" && "bg-red-50",
-
     className
   )
 
   const dotClasses = classNames(
-    "absolute bottom-0 right-0 rounded-full",
-    "shadow-[0_0_0_2px_rgb(255,255,255)]",
+    "absolute bottom-0 right-0 rounded-full shadow-[0_0_0_2px_rgb(255,255,255)]",
     (size === "xs" || size === "sm") ? "w-2 h-2" : "w-2.5 h-2.5",
     !statusIndicator && "hidden",
     status ? "bg-green-50" : "bg-gray-30"
   )
 
-  const iconClasses = classNames(
-    size === "xs" && "w-4",
-    size === "sm" && "w-5",
-    size === "md" && "w-5",
-    size === "lg" && "w-6",
-  )
-
-  return (  
+  return (
     <div className={classes}>
-      <span className="">
-        {
-          type === "initials" ? 
-          <p className="">{initials}</p> :
-          <DocumentIcon className={iconClasses} />
-        }
-        <p className={dotClasses}></p>
-      </span>
+      { type === "initials" ?
+        <span>{initials}</span>:
+        <DocumentIcon className="w-full p-2" />
+      }
+      <div className={dotClasses}></div>
     </div>
   )
 }
@@ -68,7 +56,8 @@ IdentifierIcon.defaultProps = {
   size: "lg",
   type: "initials",
   statusIndicator: true,
-  status: true
+  status: true,
+  initials: ""
 }
 
 IdentifierIcon.propTypes = {
