5ddfb7eabc68c60add44afa94f8900d0933d12f2
Angel <82048582+angelangelov88@users.noreply.github.com>
2022-11-11 17:24:03 +0000

    amplify install

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "uploadapptest39a132ce": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "uploadTest": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
